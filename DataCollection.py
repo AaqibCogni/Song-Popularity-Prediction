c3e324622abc2c161b8ea9c1a407064fb8498980
Aaqib Naqati <naqatiaaqib@gmail.com>
2022-02-17 16:53:10 +0400

    initial commit

--- DataCollection.py
+++ DataCollection.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import lyricsgenius as lg
 import billboard
 import numpy
@@ -20,12 +14,9 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-# In[ ]:
-
-
 """
 Function collect_hot100_from_billboard collects the Top 100 songs from billboard platform from a Data range passed to it as parameters.
-Parameters: 
+Parameters:
 1. start_year
 2. end_year
 
@@ -38,7 +29,7 @@ def collect_hot100_from_billboard(start_year,end_year):
         date = str(i) + '-' + '01' + '-' + '01'
         chart = billboard.ChartData('hot-100', date = date)
         for j in range(0,100):
-            row = { 
+            row = {
                 'Song Title': chart.entries[j].title,
                 'Artist':chart.entries[j].artist,
                 'Year': i
@@ -46,10 +37,6 @@ def collect_hot100_from_billboard(start_year,end_year):
             dataset = dataset.append(row, ignore_index=True)
     return dataset
 all_songs = collect_hot100_from_billboard(2000,2020)
-
-
-# In[ ]:
-
 
 """
 Function get_lyrics extracts lyrics of the songs present in all songs data based on Song Title and Artist Name
@@ -82,13 +69,9 @@ def get_lyrics(all_songs):
         all_song_data = all_song_data.append(row, ignore_index=True)
     end_time = datetime.now()
     print("\nCompleted at {}".format(start_time))
-    print("Total time to collect: {}".format(end_time - start_time)) 
+    print("Total time to collect: {}".format(end_time - start_time))
     return all_song_data
 all_song_data = get_lyrics(all_songs)
-
-
-# In[ ]:
-
 
 """
 Function to get the popularity score of the songs from spotify based on track and artist
@@ -110,7 +93,7 @@ def is_similar(A, B, rate=0.75):
 def get_popularity(track, artist):
     offset = -1
     max_popularity = 0
-    
+
     while True:
 
         if offset == API_MAX_OFFSET:
@@ -120,11 +103,11 @@ def get_popularity(track, artist):
             results = sp.search(q=f"track:{track}", type='track', limit=50, offset=offset)
         except SpotifyException as e:
             break
-        items = results['tracks']['items']    
+        items = results['tracks']['items']
         size = len(items)
         if size == 0:
             break
-        offset += size   
+        offset += size
         for item in items:
             uri = item["uri"]
             name = item["name"]
@@ -157,4 +140,3 @@ if __name__ == "__main__":
 
 
 all_song_data.to_excel(r'D\....\all_song_data_with_popularity',index=False)
-
